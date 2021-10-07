b28d66f3b8521b040afb23a65e03a25ccef9d726
Pablo Laso Cortabitarte <pablo.lasocortabitarte@telefonica.com>
2021-10-07 03:27:50 +0200

    debug

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -42,6 +42,8 @@ module.exports.loginCb = async (req, res, next) => {
         userName: user.login,
       });
 
+      console.log({ createdUser });
+
       res.json({
         access_token: signToken(createdUser),
       });
