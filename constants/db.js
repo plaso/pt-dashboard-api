43af34b9769dc047e059ba873711225a97024382
Pablo Laso Cortabitarte <pablo.lasocortabitarte@telefonica.com>
2021-10-07 04:09:04 +0200

    mongo db uri

--- constants/db.js
+++ constants/db.js
@@ -1,8 +1,7 @@
 require("dotenv").config();
 
 const DB_NAME = "dashboard";
-const URI = process.env.MONGODB_URI || "mongodb://localhost:27017";
-const DB_URI = `${URI}/${DB_NAME}`;
+const URI = process.env.MONGODB_URI || "mongodb://localhost:27017/dashboard";
 
 module.exports.dbName = DB_NAME;
-module.exports.db = DB_URI;
+module.exports.db = URI;
